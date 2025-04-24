53b8f280c6eda3c28593573ad6fc669a4936f628
Andile Sithole <andilenicolas6@gmail.com>
2025-04-24 13:37:29 +0200

    service a successful build.

--- A/src/test/java/com/service/A/AApplicationTests.java
+++ A/src/test/java/com/service/A/AApplicationTests.java
@@ -1,13 +1,11 @@
 package com.service.A;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
